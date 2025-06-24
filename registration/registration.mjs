ff95e8422891be3168d2c9b0c1b0337a7cdeadd9
William Kan <22091wk@hvhs.school.nz>
2025-06-24 13:03:48 +0000

    did 2h

--- registration/registration.mjs
+++ registration/registration.mjs
@@ -1,19 +1,20 @@
 console.log("%cregistration.mjs running", "color: blue;")
-var UID;
-
+var UID1;
 function saveSubmit() {
-    console.log("%cIt works", "color: blue;")
+    var UID1 = sessionStorage.getItem("UID");
+    console.log("%cIt works", "color: blue;");
 
     let name = document.getElementById("name").value;
-    let dateOfBirth = document.getElementById("dateOfBirth").value;
-    UID = sessionStorage.getItem("UID");
-    if(UID != null) {
+    let age = document.getElementById("age").value;
+
+    if(UID1 !== 'undefined') {
         window.location.replace("../index.html");
+
     } else {
         document.getElementById("statusMessage2").innerHTML = "You Are Not Logged In With Google!";
 
     }
     sessionStorage.setItem("Name", name);
-    sessionStorage.setItem("dateOfBirth", dateOfBirth);
+    sessionStorage.setItem("age", age);
       
 }
